1e1caf4beb9a3e5921c83c7c1d70c7ebeded3925
mohan <chandramohan.sanwal@anviam.com>
2023-06-29 18:49:40 +0530

    change

--- routes/web.php
+++ routes/web.php
@@ -304,8 +304,15 @@ Route::post('/payment/response',[PaymentController::class,'paymentResponse'])->n
 Route::post('/payment',[PaymentController::class,'paymentData'])->name("payment");
 
 
+
+
+
+
 Route::post("/payment/process/data",[PaymentController::class,'paymentProcess'])->name('payment.process.data');
 Route::get("/payment/process",[PaymentController::class,'index'])->name('payment.process');
+
+
+
 
 
 
