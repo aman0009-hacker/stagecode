3de689052966c4eede7847e3b057b7c895c44cf7
mohan <chandramohan.sanwal@anviam.com>
2023-07-12 13:04:55 +0530

    change

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,59 +1,51 @@
-let allbutton=document.getElementsByClassName('allbtn') ;
-let allitem=document.getElementsByClassName('allitems') ; 
+Array.from(document.getElementsByClassName('allbtn')).forEach(element => {
+   element.addEventListener('click', function () {
 
+      $('#OrderForm')[0].reset();
 
-Array.from(allbutton).forEach(element =>  {
-     element.addEventListener('click',function()
-     {
-  
-         $('#OrderForm')[0].reset();
-        
-       for(let a=allitem.length-1;a>0;a--)
-       {
-          allitem[a].remove() ;
-          num=1 ;
-       }
-        let attr=element.getAttribute('id');
-        //alert(attr);
-        document.getElementById("modalIdInput").value = attr;
+      for (let a = document.getElementsByClassName('allitems').length - 1; a > 0; a--) {
+         document.getElementsByClassName('allitems')[a].remove();
+         num = 1;
+      }
+      let attr = element.getAttribute('id');
+      //alert(attr);
+      document.getElementById("modalIdInput").value = attr;
 
-        $('#openthemodal').modal('show') ;
-    })
-    
+      $('#openthemodal').modal('show');
+   })
+
 });
 
 
-let num=2;
-function imagesAdd()
- {
-   
-    if(num<=2)
- {
-    let grab=document.getElementById('allfiles');
-    let add=document.createElement('input');
-    add.setAttribute('type','file');
-    add.setAttribute('name','files[]');
-    add.setAttribute('id','mainfiles');
-    add.setAttribute('class','allitems');
-  
-   
-    grab.append(add);
-    num++;
-     }
-    else
-     {
-   alertify.error('Field limit exceed');
-     }
+let num = 2;
+function imagesAdd() {
 
- }
-function imagesRemove()
+   if (num <= 2) {
+      let grab = document.getElementById('allfiles');
+      let add = document.createElement('input');
+      add.setAttribute('type', 'file');
+      add.setAttribute('name', 'files[]');
+      add.setAttribute('id', 'mainfiles');
+      add.setAttribute('class', 'allitems');
 
-{
-    let grab=document.querySelectorAll('input[type="file"]') ;
-    for(let a=grab.length-1;a>0;a-- )
-    {
-        grab[a].remove() ;
-        num-- ;
-     break ; 
-  }
+
+      grab.append(add);
+      num++;
+   }
+   else {
+      alertify.error('Field limit exceed');
+   }
+
 }
+function imagesRemove() {
+   let grab = document.querySelectorAll('input[type="file"]');
+   for (let a = grab.length - 1; a > 0; a--) {
+      grab[a].remove();
+      num--;
+      break;
+   }
+}
+
+
+
+ 
