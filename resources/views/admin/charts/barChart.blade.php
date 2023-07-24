87fa7bedab0ae81977db9929a89540c161729578
mohan <chandramohan.sanwal@anviam.com>
2023-07-24 18:06:53 +0530

    change

--- resources/views/admin/charts/barChart.blade.php
+++ resources/views/admin/charts/barChart.blade.php
@@ -1,43 +1,49 @@
 <html>
+  <head>
 
-<head>
-
-  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
   <style>
-    @import url('https://fonts.googleapis.com/css?family=Poppins');
-    * {
-      font-family: 'Poppins', sans-serif;
-    }
-    #chart {
-      width: 100%;
-      margin: 35px auto;
-      opacity: 0.9;
-    }
-    #timeline-chart .apexcharts-toolbar {
-      opacity: 1;
-      border: 0;
-    }
-   .apexcharts-zoomin-icon,
-    .apexcharts-zoomout-icon,
-    .apexcharts-zoom-icon.apexcharts-selected,
-    .apexcharts-reset-icon,
-    .apexcharts-pan-icon {
-      display: none;
-    }
+@import url('https://fonts.googleapis.com/css?family=Poppins');
+
+* {
+  font-family: 'Poppins', sans-serif;
+}
+
+#chart {
+  width: 100%;
+  margin: 35px auto;
+  opacity: 0.9;
+}
+
+#timeline-chart .apexcharts-toolbar {
+  opacity: 1;
+  border: 0;
+}
+
+.apexcharts-zoomin-icon, .apexcharts-zoomout-icon,.apexcharts-zoom-icon.apexcharts-selected, .apexcharts-reset-icon,.apexcharts-pan-icon {
+    display: none;
+}
+
+
   </style>
-</head>
-<body>
-  <!--Div that will hold the pie chart-->
-  <form>
-    @csrf
+  </head>
+
+
+  <body>
+    <!--Div that will hold the pie chart-->
+    <form>
+        @csrf
     <div id="chart"></div>
-  </form>
-  <script>
+    </form>
+
+    <script>
+
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
+
     $(function () {
   $.ajax({
       url:'/chartApproveStatus',
@@ -45,7 +51,7 @@
       dataType:'JSON',
       success:function(response)
       {
-                // alert(response.data[]);
+
         var options = {
         chart: {
             height: 420,
@@ -77,28 +83,31 @@
         title: {
             text: "Active User"
         },
-          xaxis: {
+        xaxis: {
+
         },
         yaxis: [
             {
       title: {
-          text: "Approved"
+          text: ""
         }
     },
     {
         opposite: true,
         title: {
-            text: "New"
+            text: ""
       }
     }
         ]
         }
+
+
     var chart = new ApexCharts(document.querySelector("#chart"), options);
+
     chart.render();
     }
 })
     });
-  </script>
-</body>
-
+    </script>
+  </body>
 </html>
--- resources/views/admin/charts/waveChart.blade.php
+++ resources/views/admin/charts/waveChart.blade.php
@@ -1,102 +1,116 @@
 <html>
 
 <head>
-  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-  <style>
-    @import url(https://fonts.googleapis.com/css?family=Roboto);
 
-    body {
-      font-family: Roboto, sans-serif;
-    }
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <style>
+        @import url(https://fonts.googleapis.com/css?family=Roboto);
 
-    #chart {
-      max-width: 650px;
-      /* margin: 35px auto; */
-    }
-  </style>
+        body {
+            font-family: Roboto, sans-serif;
+        }
+
+        #chart {
+            max-width: 650px;
+            /* margin-top: 40px!important; */
+        }
+        #chart100{
+            margin-top: 40px;
+        }
+
+        #chart100 .apexcharts-toolbar {
+        top: -50px!important;
+
+}
+    </style>
 </head>
 
+
 <body>
-  <form>
-    @csrf
+
+
     <div id="chart100" mt-3></div>
     <script>
-      var options = {
-      chart: {
-        height: 350,
-        type: 'area',
-        stacked: true,
-        events: {
-          selection: function(chart, e) {
-            console.log(new Date(e.xaxis.min) )
-          },
-          markerClick: function(a, b, c) {
-            console.info(c);
-            alert(c.w.globals.seriesNames[c.seriesIndex])
-          }
-        },
-      },
-      colors: ['#008FFB', '#00E396', '#CED4DC'],
-      dataLabels: {
-          enabled: false
-      },
-      stroke: {
-        curve: 'smooth'
-      },
-      series: [{
-          name: 'South',
-          data: generateDayWiseTimeSeries(new Date('11 Feb 2017 GMT').getTime(), 20, {
-            min: 10,
-            max: 60
-          })
-        },
-        {
-          name: 'North',
-          data: generateDayWiseTimeSeries(new Date('11 Feb 2017 GMT').getTime(), 20, {
-            min: 10,
-            max: 20
-          })
-        },
-        {
-          name: 'Central',
-          data: generateDayWiseTimeSeries(new Date('11 Feb 2017 GMT').getTime(), 20, {
-            min: 10,
-            max: 15
-          })
-        }
-      ],
-      fill: {
-        type: 'gradient',
-        gradient: {
-          opacityFrom: 0.6,
-          opacityTo: 0.8,
-        }
-      },
-      legend: {
-        position: 'top',
-        horizontalAlign: 'left'
-      },
-      xaxis: {
-        type: 'datetime'
-      },
-    }
-    var chart = new ApexCharts(
-      document.querySelector("#chart100"),
-      options
-    );
-    chart.render();
-    function generateDayWiseTimeSeries(baseval, count, yrange) {
-      var i = 0;
-      var series = [];
-      while (i < count) {
-        var x = baseval;
-        var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-       series.push([x, y]);
-        baseval += 86400000;
-        i++;
+
+
+
+        $(function() {
+            $.ajax({
+                url: '/totalOrdersAmount',
+                type: 'GET',
+                dataType: 'JSON',
+                success: function(response)
+                {
+                    const month=response.data.month;
+                    const total =response.data.total;
+
+                    var options = {
+                        chart: {
+                            height: 350,
+                            type: 'area',
+                            stacked: true,
+                            events: {
+                                selection: function(chart, e) {
+                                    console.log(new Date(e.xaxis.min))
+                                },
+                                markerClick: function(a, b, c) {
+                                    console.info(c);
+                                    // alert(c.w.globals.seriesNames[c.seriesIndex])
+                                }
+                            },
+
+                        },
+                        colors: ['#008FFB', '#00E396', '#CED4DC'],
+                        dataLabels: {
+                            enabled: false
+                        },
+                        stroke: {
+                            curve: 'smooth'
+                        },
+
+                        series: [
+                            {
+                                name: "Total Amount",
+                                data:total
+                            },
+
+
+                        ],
+                        fill: {
+                            type: 'gradient',
+                            gradient: {
+                                opacityFrom: 0.6,
+                                opacityTo: 0.8,
+                            }
+                        },
+                        legend: {
+                            position: 'top',
+                            horizontalAlign: 'left'
+                        },
+                        xaxis: {
+                            categories:month
+                        },
+                        yaxis:[{
+
+                            title: {
+                                    text: "Amount",
+                                    style: {
+                                        colors: "008FFB"
+                                    }
+                        }
       }
-      return series;
-    }
+                        ]
+                    }
+
+                    var chart = new ApexCharts(
+                        document.querySelector("#chart100"),
+                        options
+                    );
+
+                    chart.render();
+                }
+            });
+        });
     </script>
 </body>
 
