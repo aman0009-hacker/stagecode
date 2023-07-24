a0cf8db944359630a763509c9b6a3ea0c03d0793
mohan <chandramohan.sanwal@anviam.com>
2023-07-24 11:27:02 +0530

    change

--- database/migrations/2023_07_22_121729_add_psiec_address_ludhiana_to_address_table.php
+++ database/migrations/2023_07_22_121729_add_psiec_address_ludhiana_to_address_table.php
@@ -14,14 +14,16 @@ return new class extends Migration
     {
         Schema::table('address', function (Blueprint $table) {
             // $table->json('psiec_address_ludhiana')->default('{"psiec_biilling_name": "Punjab Small Industries & Export Corp. Ind.", "psiec_billing_area": "Area-B", "psiec_biilling_city": "Ludhiana", "psiec_biilling_gst": "03AABCP1602M1ZT", "psiec_biilling_state": "Punjab", "psiec_biilling_code": "03", "psiec_biilling_cin": "U51219CH9162SGC002427"}');
-            $table->json('psiec_address_ludhiana')->default(\DB::raw('JSON_OBJECT(
-                "psiec_biilling_name", "Punjab Small Industries & Export Corp. Ind.",
-                "psiec_billing_area", "Area-B",
-                "psiec_biilling_city", "Ludhiana",
-                "psiec_biilling_gst", "03AABCP1602M1ZT",
-                "psiec_biilling_state", "Punjab",
-                "psiec_biilling_code", "03",
-                "psiec_biilling_cin", "U51219CH9162SGC002427"
+            $table->json('psiec_address_ludhiana')->default(\DB::raw('JSON_ARRAY(
+                JSON_OBJECT(
+                    "psiec_biilling_name", "Punjab Small Industries & Export Corp. Ind.",
+                    "psiec_billing_area", "Area-B",
+                    "psiec_biilling_city", "Ludhiana",
+                    "psiec_biilling_gst", "03AABCP1602M1ZT",
+                    "psiec_biilling_state", "Punjab",
+                    "psiec_biilling_code", "03",
+                    "psiec_biilling_cin", "U51219CH9162SGC002427"
+                )
             )'));
         });
     }
