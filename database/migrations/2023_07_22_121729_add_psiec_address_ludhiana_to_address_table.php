0b3e30fdfab3237c0f656d00d765ceaae94c27ba
mohan <chandramohan.sanwal@anviam.com>
2023-07-24 11:21:08 +0530

    change

--- database/migrations/2023_07_22_121729_add_psiec_address_ludhiana_to_address_table.php
+++ database/migrations/2023_07_22_121729_add_psiec_address_ludhiana_to_address_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,7 +13,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('address', function (Blueprint $table) {
-            $table->json('psiec_address_ludhiana')->default('{"psiec_biilling_name": "Punjab Small Industries & Export Corp. Ind.", "psiec_billing_area": "Area-B", "psiec_biilling_city": "Ludhiana", "psiec_biilling_gst": "03AABCP1602M1ZT", "psiec_biilling_state": "Punjab", "psiec_biilling_code": "03", "psiec_biilling_cin": "U51219CH9162SGC002427"}');
+            // $table->json('psiec_address_ludhiana')->default('{"psiec_biilling_name": "Punjab Small Industries & Export Corp. Ind.", "psiec_billing_area": "Area-B", "psiec_biilling_city": "Ludhiana", "psiec_biilling_gst": "03AABCP1602M1ZT", "psiec_biilling_state": "Punjab", "psiec_biilling_code": "03", "psiec_biilling_cin": "U51219CH9162SGC002427"}');
+            $table->json('psiec_address_ludhiana')->default(\DB::raw('JSON_OBJECT(
+                "psiec_biilling_name", "Punjab Small Industries & Export Corp. Ind.",
+                "psiec_billing_area", "Area-B",
+                "psiec_biilling_city", "Ludhiana",
+                "psiec_biilling_gst", "03AABCP1602M1ZT",
+                "psiec_biilling_state", "Punjab",
+                "psiec_biilling_code", "03",
+                "psiec_biilling_cin", "U51219CH9162SGC002427"
+            )'));
         });
     }
 
