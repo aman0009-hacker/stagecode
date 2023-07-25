430bc67a86663347a7e93754402891a022759f1e
mohan <chandramohan.sanwal@anviam.com>
2023-07-25 13:07:50 +0530

    change

--- database/migrations/2023_07_21_150132_create_table_invoice.php
+++ database/migrations/2023_07_21_150132_create_table_invoice.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,7 +14,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('invoice', function (Blueprint $table) {
-            $table->uuid('id')->primary()->default(Str::uuid());
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
             //$table->uuid('id')->primary();
             $table->text('delivery_terms')->nullable();
             $table->date('invoice_date')->nullable();
