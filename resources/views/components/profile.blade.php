2617599e432e583106ac0453c7ea19f560b799db
mohan <chandramohan.sanwal@anviam.com>
2023-07-26 15:57:46 +0530

    change

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.main_account')
 @section('content')
 {{--
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css"
