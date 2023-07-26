ee0340ea962b920b6820f21cf4c2c06fdec3e4e6
mohan <chandramohan.sanwal@anviam.com>
2023-07-26 23:53:39 +0530

    change

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -19,22 +19,25 @@ class HomeController extends Controller
             ->description('Admin Portal')
             // ->row(Dashboard::title())
             ->row(function (Row $row) {
-                $row->class('justify-content-center align-items-center h-100');
-                $row->column(4, function (Column $column) {
+                $row->class('justify-content-center align-items-center h-50');
+                $row->column(3, function (Column $column) {
                     $column->append(new Box('Users Records', view('admin.charts.barChart')));
                 });
-                $row->column(4, function (Column $column) {
-                    $column->append(new Box('Users Count Records ', view('admin.charts.donutChart')));
-                });
-                $row->column(4, function (Column $column) {
+
+                $row->column(5, function (Column $column) {
                     $column->append(new Box('Yard Records', view('admin.charts.lineChart')));
                 });
-                $row->column(7, function (Column $column) {
+                $row->column(4, function (Column $column) {
                     $column->append(new Box('Order Amount Records', view('admin.charts.waveChart')));
                 });
-                $row->column(5, function (Column $column) {
+                $row->column(6, function (Column $column) {
+                    $column->append(new Box('Users Count Records ', view('admin.charts.donutChart')));
+                });
+                $row->column(6, function (Column $column) {
                     $column->append(new Box('Order Count Records', view('admin.charts.pieChart')));
-                  });
+                });
+
+
             });
 
     }
--- resources/views/admin/charts/barChart.blade.php
+++ resources/views/admin/charts/barChart.blade.php
@@ -2,103 +2,88 @@
 <html>
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pie Chart Example</title>
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Poppins');
+  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Poppins');
 
-        * {
-            font-family: 'Poppins', sans-serif;
-        }
+    * {
+      font-family: 'Poppins', sans-serif;
+    }
 
-        #chart {
-            width: 100%;
-            margin: 75px auto !important;
-            opacity: 0.9;
-        }
+    #chart {
+      width: 100%;
+      margin: 0 auto;
+      opacity: 0.9;
+    }
 
-        #timeline-chart .apexcharts-toolbar {
-            opacity: 1;
-            border: 0;
-        }
+    .apexcharts-toolbar {
+      opacity: 1;
+      border: 0;
+    }
 
-        #chart .apexcharts-toolbar {
-            top: -65px !important;
-        }
-
-        .apexcharts-zoomin-icon,
-        .apexcharts-zoomout-icon,
-        .apexcharts-zoom-icon.apexcharts-selected,
-        .apexcharts-reset-icon,
-        .apexcharts-pan-icon {
-            display: none;
-        }
-    </style>
+    .apexcharts-zoomin-icon,
+    .apexcharts-zoomout-icon,
+    .apexcharts-zoom-icon.apexcharts-selected,
+    .apexcharts-reset-icon,
+    .apexcharts-pan-icon {
+      display: none;
+    }
+  </style>
 </head>
 
 <body>
-    <!--Div that will hold the pie chart-->
-    <form>
-        @csrf
-        <div id="chart"></div>
-    </form>
+  <!--Div that will hold the pie chart-->
+  <form>
+    @csrf
+    <div id="chart"></div>
+  </form>
 
-    <script>
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
+  <script>
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      }
+    });
 
-        $(function () {
-            $.ajax({
-                url: '/chartApproveStatus',
-                type: 'POST',
-                dataType: 'JSON',
-                success: function (response) {
-                    const data = response.data;
-                    const error = response.data.length;
+    $(function() {
+      $.ajax({
+        url: '/chartApproveStatus',
+        type: 'POST',
+        dataType: 'JSON',
+        success: function(response) {
+          const data = response.data;
 
-                    const graph = document.getElementById('chart');
-                    if (error === 0) {
-                        graph.style.display = 'none';
-                    } else {
-                        graph.style.display = 'block';
+          const error = response.data.length;
 
-                        var options = {
-                            series: data,
-                            chart: {
-                                width: 420,
-                                type: 'pie',
-                                toolbar: {
-                                    show: true
-                                }
-                            },
-                            labels: ['Approved', 'New', 'Rejected'],
-                            legend: {
-                                position: 'bottom' // Set the legend position to bottom
-                            },
-                            responsive: [{
-                                breakpoint: 480,
-                                options: {
-                                    chart: {
-                                        width: 200
-                                    },
-                                    legend: {
-                                        position: 'bottom' // Set the legend position to bottom for smaller screens
-                                    }
-                                }
-                            }]
-                        };
+          const graph = document.getElementById('chart');
 
-                        var chart = new ApexCharts(document.querySelector("#chart"), options);
-                        chart.render();
-                    }
+          if (error === 0) {
+            graph.style.display = 'none';
+          } else {
+            graph.style.display = 'block';
+
+            var options = {
+              series: data,
+              chart: {
+                width: '100%',
+                type: 'pie',
+                toolbar: {
+                  show: true
                 }
-            });
-        });
-    </script>
+              },
+              labels: ['Approved', 'New', 'Rejected'],
+              legend: {
+                position: 'bottom'
+              }
+            };
+
+            var chart = new ApexCharts(document.querySelector("#chart"), options);
+            chart.render();
+          }
+        }
+      })
+    });
+  </script>
 </body>
 
 </html>
